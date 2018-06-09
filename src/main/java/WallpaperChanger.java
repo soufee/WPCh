82b60dca87661b3c6e47d3ba2dcc1d8e1724f339
soufee <soufee@mail.ru>
2018-06-09 23:19:48 +0300

    manifest

--- src/main/java/WallpaperChanger.java
+++ src/main/java/WallpaperChanger.java
@@ -1,9 +1,4 @@
-import com.sun.jna.Library;
-import com.sun.jna.Native;
-import com.sun.jna.win32.W32APIOptions;
-
 import javax.imageio.ImageIO;
-import javax.swing.filechooser.FileSystemView;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -13,57 +8,51 @@ import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
 import java.nio.file.Files;
-
 import java.nio.file.Paths;
 
-
-
 public class WallpaperChanger {
+    private static final String sourceFile = "screen.jpg";
+    private static final String sourceLink = "http://v.img.com.ua/b/600x500/7/c6/f261e6e5bb56d3564c8e49d4662d9c67.jpg";
+    private static User32 u;
+
     private static void downloadFileFromURL(String urlString, File destination) {
-        try {
+        try (FileOutputStream fos = new FileOutputStream(destination)) {
             URL website = new URL(urlString);
             ReadableByteChannel rbc;
             rbc = Channels.newChannel(website.openStream());
-            FileOutputStream fos = new FileOutputStream(destination);
             fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
-            fos.close();
             rbc.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static void ChangeDesktopBackgroundOnWindows(String ruta) {
-        User32.INSTANCE.SystemParametersInfo(0x0014, 0, ruta, 1);
+    private void ChangeDesktopBackgroundOnWindows(String ruta) throws ClassNotFoundException {
+        u = User32.INSTANCE;
+        u.SystemParametersInfo(0x0014, 0, ruta, 1);
     }
 
-    private static File getHomeDir() {
-        FileSystemView fsv = FileSystemView.getFileSystemView();
-        return fsv.getHomeDirectory();
-    }
-
-    private static BufferedImage grabScreen() {
+    private static void grabScreen() {
         try {
-            System.out.println("Снимаем скрин с экрана");
+            System.out.println("screening...");
             BufferedImage image = new Robot().createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
-            ImageIO.write(image, "jpg", new File("screen.jpg"));
-           } catch (Exception e) {
-
+            ImageIO.write(image, "jpg", new File(sourceFile));
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        return null;
+
     }
 
-    public static void main(String[] args) throws IOException {
-
-        downloadFileFromURL("http://v.img.com.ua/b/600x500/7/c6/f261e6e5bb56d3564c8e49d4662d9c67.jpg", new File("1.jpg"));
-        String ruta = "1.jpg";
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
         grabScreen();
         boolean f = Files.isRegularFile(Paths.get("screen.jpg"));
+        String ruta = "";
+        if (!f) {
+            downloadFileFromURL(sourceLink, new File(sourceFile));
+            ruta = "1.jpg";
+        }
         String filepath = (f) ? "screen.jpg" : ruta;
-        System.out.println(filepath);
-        System.out.println(f);
-        ChangeDesktopBackgroundOnWindows(filepath);
-        System.out.println("Экран сменен ");
+        new WallpaperChanger().ChangeDesktopBackgroundOnWindows(filepath);
 
     }
 
